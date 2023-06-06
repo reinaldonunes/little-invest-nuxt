48cd30ca0367783560b5a8052436c3ea0582d2a3
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-06-06 16:49:15 -0300

    feat: added default index and imported Hero component

---
+++ components/Hero.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+<template>
+  <div class="m-auto max-w-[1440px] w-full items-center px-4 py-16 sm:py-0 grid  grid-col-1 sm:grid-cols-2 gap-10">
+    <div class="flex flex-col">
+      <h5 class="leading-snug text-center sm:text-start text-[28px] md:text-[36px] lg:text-[48px]">Planeje o seu futuro sem perder tempo no presente</h5>
+      <NuxtLink
+        to="/register"
+        class="w-full sm:w-fit justify-center px-6 py-3 mt-5 font-[500] flex rounded-md text-white bg-[#4455DD] hover:bg-[#4411DD] ease-in duration-100"  
+      >Investir agora</NuxtLink>
+    </div>
+    <div>
+      <figure>
+        <img src="/svg/notre-dame.svg" alt="Notre Drame" title="Investir agora" />
+      </figure>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script>
+  import Hero from '~/components/Header.vue'
+
+  export default defineComponent({
+    name: 'Index',
+    components: {
+      Hero
+    }
+  })
+</script>
+<template>
+  <Hero />
+</template>
