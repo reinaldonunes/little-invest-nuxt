1ced52dc8e3ee7a14f75195da68e2b7500edebad
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-06-06 17:33:14 -0300

    feat: added benefits component

---
+++ components/Benefits.vue
@@ -0,0 +1,30 @@
+<script>
+  export default defineComponent({
+    name: 'Benefits',
+    data(){
+      return{
+        benefits: [
+          { id: 1, name: 'Acesso Online', description: 'Acesse a nossa plataforma e escolha o seu plano', image: '/svg/monitor-window.svg' },
+          { id: 2, name: 'Rendimento Fácil', description: 'Espere até a data de resgate e acompanhe o rendimento', image: '/svg/monitor-loading-progress.svg' },
+          { id: 3, name: 'Resgate Simples', description: 'Retire o rendimento e gaste parte em viagens e compras', image: '/svg/monitor-cash-credit-card.svg' }
+        ]
+      }
+    }
+  })
+</script>
+
+<template>
+  <div class="py-16 px-4">
+    <div class="m-auto max-w-[1440px] w-full items-center">
+      <span class="flex w-full justify-center text-center leading-snug text-[36px] text-gray-900">Mais fácil que gastar dinheiro</span>
+
+      <div class="mt-5 mb-5 grid sm:grid-cols-1 md:grid-cols-3 gap-10 justify-center items-center"> 
+        <section class="flex flex-col p-10 rounded-md text-center" v-for="item in benefits" :key="item.id">
+          <img :src="item.image" />
+          <span class="font-semibold text-[18px] mt-3 mb-1">{{ item.name }}</span>
+          <span>{{ item.description }}</span>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <script>
   import Hero from '~/components/Header.vue'
   import Investiments from '~/components/Investiments.vue'
+  import Benefits from '~/components/Benefits.vue'
 
   export default defineComponent({
     name: 'Index',
     components: {
       Hero,
-      Investiments
+      Investiments,
+      Benefits
     }
   })
 </script>
 <template>
   <Hero />
   <Investiments />
+  <Benefits />
 </template>
