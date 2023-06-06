5ed08844c221dd2d93830110ffccbf614c29b84e
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-06-06 16:12:49 -0300

    feat: added tailwind css and defined layout base to app

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
+<script>
+  import Header from '~/components/Header.vue'
+
+  export default defineComponent({
+    name: 'App',
+    components:{
+      Header
+    }
+  })
+</script>
 <template>
   <div>
+    <Header />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
