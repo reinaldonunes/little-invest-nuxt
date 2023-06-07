d35b20da782d5aeccbcf33eafa1a246b4dc396ba
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-06-06 22:22:49 -0300

    feat: added head app config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Little Invest App',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
+  
   modules: [
     '@nuxtjs/tailwindcss'
   ],
